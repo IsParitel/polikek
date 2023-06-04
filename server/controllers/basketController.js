2cc34e564a878e296c3704331929963e5d4863f5
IsParitel <isparitelvaper@yandex.ru>
2023-06-04 18:56:01 +0300

    da

--- server/controllers/basketController.js
+++ server/controllers/basketController.js
@@ -1,23 +1,23 @@
-const {Basket, BasketDevice, Device} = require('../models/models')
-const {Sequelize} = require('sequelize')
+const { Basket, BasketDevice, Device } = require('../models/models');
+const { Sequelize } = require('sequelize');
 const ApiError = require('../error/ApiError');
+
 class BasketController {
-
     async getOne(req, res) {
-        const {id} = req.user
-        const basket = await Basket.findOne(
-            {
-                where: {userId: id}
+        const { id: userId } = req.user;
+
+        // Создаем новую корзину для пользователя
+        const basket = await Basket.create({ userId });
+
+        const basketDevices = await BasketDevice.findAll({ where: { basketId: basket.id } });
+        const devices = await Device.findAll({
+            where: {
+                id: { [Sequelize.Op.or]: basketDevices.map(({ deviceId }) => deviceId) },
             },
-        )
-        const basketDevices = await BasketDevice.findAll({where: {basketId: basket.id}})
-        const devices = await Device.findAll({where: {
-            id: {[Sequelize.Op.or]: basketDevices.map(({deviceId}) => deviceId)}
-        }})
+        });
 
-        return res.json({...basket.dataValues, devices })
+        return res.json({ ...basket.dataValues, devices });
     }
-
 }
 
-module.exports = new BasketController()
+module.exports = new BasketController();
